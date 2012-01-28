3440f689e5103dddf3d159068ea163f47408fc42
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-01-28 16:07:06 -0600

    ruby 1.9 style hash, yo

--- lib/redismq/client.rb
+++ lib/redismq/client.rb
@@ -16,7 +16,7 @@ module RedisMQ
     def connect_to_redis
       url = ENV['REDISMQ_URL'] || ENV['REDISTOGO_URL'] || "redis://localhost:6379"
       uri = URI.parse(url)
-      Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+      Redis.new(host: uri.host, port: uri.port, password: uri.password)
     end
 
     def pop(queue)
