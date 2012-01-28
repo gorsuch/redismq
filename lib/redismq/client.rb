4c7b517ee0ab06dea9056c829f717c7fc85830a3
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-01-28 16:07:53 -0600

    more 1.9-ish hashes

--- lib/redismq/client.rb
+++ lib/redismq/client.rb
@@ -10,7 +10,7 @@ module RedisMQ
 
     def bpop(queue, timeout=0)
       (queue, message) = redis.blpop(queue_key(queue), timeout)
-      JSON.parse(message, :symbolize_names => true)
+      JSON.parse(message, symbolize_names: true)
     end
 
     def connect_to_redis
@@ -22,7 +22,7 @@ module RedisMQ
     def pop(queue)
       result = redis.lpop(queue_key(queue))
       return nil if result.nil?
-      JSON.parse(result, :symbolize_names => true)
+      JSON.parse(result, symbolize_names: true)
     end
 
     def publish(topic, payload)
